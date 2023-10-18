ee21c445183988e07466afbff8befd627be49ded
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2023-10-19 00:42:22 +0530

    You performed better than 78.85% Runtime 181 ms

---
+++ CheckArmstrong/CheckArmstrong.py
@@ -0,0 +1,24 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+n = int(input())
+siz = len(str(n))
+tot=0
+q=n
+while q>0:
+    r=q%10
+    tot=tot+(r**siz)
+    q=q//10
+
+    # print(r,tot)
+
+
+
+if tot==n:
+    print("true")
+else:
+    print("false")
+
+
