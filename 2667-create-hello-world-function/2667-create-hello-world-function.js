0c77f1bc3bb546e2d156b2ae3833205c8debb538
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2024-03-18 21:20:54 +0530

    Time: 49 ms (64.10%), Space: 48.8 MB (39.06%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,13 @@
+
+
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
