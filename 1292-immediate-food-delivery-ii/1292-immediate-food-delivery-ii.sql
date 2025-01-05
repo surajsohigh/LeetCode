0208d47a35b84453f450d4dcbbb0fee7446dbd52
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2025-01-05 21:43:19 +0530

    Time: 1725 ms (7.5%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,14 +1,27 @@
-SELECT 
-  ROUND(SUM(preferred = 'immediate') * 100.0 / COUNT(*), 2) AS immediate_percentage
-FROM (
-  SELECT
-    customer_id,
-    CASE 
-      WHEN DATEDIFF(MIN(order_date), MIN(customer_pref_delivery_date)) = 0 THEN 'immediate'
-      ELSE 'scheduled'
-    END AS preferred
-  FROM
-    Delivery
-  GROUP BY
-    customer_id
-) AS T;
+-- SELECT 
+--   ROUND(SUM(preferred = 'immediate') * 100.0 / COUNT(*), 2) AS immediate_percentage
+-- FROM (
+--   SELECT
+--     customer_id,
+--     CASE 
+--       WHEN DATEDIFF(MIN(order_date), MIN(customer_pref_delivery_date)) = 0 THEN 'immediate'
+--       ELSE 'scheduled'
+--     END AS preferred
+--   FROM
+--     Delivery
+--   GROUP BY
+--     customer_id
+-- ) AS T;
+
+select round(sum(preferred='immediate')*100/count(*), 2) as immediate_percentage
+from 
+ (SELECT
+  CASE 
+    WHEN DATEDIFF(MIN(order_date), min(customer_pref_delivery_date)) = 0 THEN 'immediate'
+    ELSE 'scheduled'
+  END AS preferred
+FROM
+  Delivery
+GROUP BY
+  customer_id
+) as T;
