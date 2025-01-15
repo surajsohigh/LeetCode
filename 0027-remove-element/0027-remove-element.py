5122acd37086197c058f37fff91346eb4c93ec7b
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2025-01-15 14:39:28 +0530

    Time: 0 ms (100%), Space: 18 MB (5.59%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,28 +1,8 @@
-from collections import deque
-
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        
-        dup=len(nums)-1
-        left=len(nums)-1
-        
-        while left >= 0:
-            if nums[left] == val:
-                print("h")
-                nums[dup], nums[left] = nums[left], nums[dup]
-                dup -= 1
-                
-            
-            left -= 1
-            
-        
-        numDup = len(nums[:dup+1])
-        print(nums, dup, numDup)
-        return numDup
-        
-        
-        
-            
-            
-        
-        
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        return count
