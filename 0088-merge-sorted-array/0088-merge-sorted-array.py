b03324eb0a5b040dd1a446881e54e4d52a581608
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2025-01-15 14:21:11 +0530

    Time: 0 ms (100%), Space: 18 MB (12.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,61 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        for i in range(n):
+            nums1[m+i] = nums2[i]
         
-        sarr1 = m 
-        sarr2 = n 
-        last = (m+n)-1 
+        return nums1.sort()
         
-        while sarr2 > 0 and sarr1 > 0:
-            
-            if nums1[sarr1-1] > nums2[sarr2-1]:
-                nums1[last] = nums1[sarr1-1]
-                sarr1 -= 1
-                
-            else:
-                nums1[last] = nums2[sarr2-1]
-                sarr2 -= 1
-            last -= 1
-            print(nums1, last)
-        
-        while sarr2 > 0:
-            nums1[last] = nums2[sarr2-1]
-            last -= 1
-            sarr2 -= 1
-                
-       
-        # i=0; j=0
-#         li = nums1[:m]
-#         idx = 0
-#         while i<n and j<m:
-#             if li[i] < nums2[j]:
-#                 nums1[idx]=li[i]
-#                 idx=idx+1
-#                 i=i+1
-#             else:
-#                 nums1[idx]=nums2[j]
-#                 idx=idx+1
-#                 j=j+1
-                
-#         if i == len(li) 
-                
-#         while i < len(li):
-#             nums1[idx] = li[i]
-#             idx = idx+1
-#             i=i+1
-            
-#         while j < len(nums2):
-#             nums1[idx] = nums2[j]
-#             idx = idx+1
-#             j=j+1
-        
-        
-
-
-#         print(nums1)
-        
-        
-            
-        
-            
-            
